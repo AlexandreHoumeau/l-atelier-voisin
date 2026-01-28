a41dad614f9faaed8eaf3aba4c9fe5216899b975
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2026-01-28 16:03:40 +0100

    fixed redirect issue

--- app/go/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET(
-    request: Request,
-    { params }: { params: { slug: string } }
-) {
-    // ici tu peux logguer plus tard si tu veux
-    return NextResponse.redirect(new URL("/", request.url))
-}
