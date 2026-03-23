3bdc9b0538eb71750603349fba2538e68bc47835
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2026-03-23 15:36:37 +0100

    small caching fixes

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,16 +2,16 @@ import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  try {
-    const secret = request.headers.get('x-sanity-secret');
-    if (secret !== process.env.SANITY_WEBHOOK_SECRET) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    try {
+        const secret = request.headers.get('x-sanity-secret');
+        if (secret !== process.env.SANITY_WEBHOOK_SECRET) {
+            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+        }
+
+        revalidatePath('/', 'layout');
+        
+        return NextResponse.json({ message: 'Cache cleared successfully!' });
+    } catch (err) {
+        return NextResponse.json({ message: 'Error clearing cache' }, { status: 500 });
     }
-
-    revalidatePath('/'); 
-
-    return NextResponse.json({ message: 'Cache cleared successfully!' });
-  } catch (err) {
-    return NextResponse.json({ message: 'Error clearing cache' }, { status: 500 });
-  }
 }
